8034652625ece9002072167d3df8206064a5b8c8
Defy M Aminuddin <defyma85@gmail.com>
2022-02-26 14:22:00 +0700

    fix

--- 36/bear/doodles/86806bbae99dbaf3447713.js
+++ 36/bear/doodles/86806bbae99dbaf3447713.js
@@ -5,8 +5,8 @@
  *     Token Hash: 0xbaF02040337B6DfD745563D259718Da3BC62Fb70
  *     Contract: https://polygonscan.com/address/0xbaF02040337B6DfD745563D259718Da3BC62Fb70#code
  * 
- *     Web Build Date: Sat Feb 26 2022 14:17:39 GMT+0700 (Western Indonesia Time)
- *     Web Version: 1.0.0-1645859859629
+ *     Web Build Date: Sat Feb 26 2022 14:20:31 GMT+0700 (Western Indonesia Time)
+ *     Web Version: 1.0.0-1645860031344
  * ******************************************export default {**************************************
  *                 
  */
